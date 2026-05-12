82d4cbc20b1d6c041f3ac70a770b5d44262c1b81
juan david quintero obregon <juanquinterosabios@gmail.com>
2026-05-12 15:02:23 -0500

    Script Test

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# Main Script
+
+# Este archivo será el punto de entrada principal para el análisis de datos del proyecto F1.
+# Aquí se incluirán las funciones principales y la lógica del programa.
+
+def main():
+    print("Bienvenido al analizador de datos de F1")
+
+if __name__ == "__main__":
+    main()
